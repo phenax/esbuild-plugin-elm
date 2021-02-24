9c257bb57976d8bbaf8cb8406870cf6823f3f30f
Akshay Nair <phenax5@gmail.com>
2021-02-24 23:40:40 +0530

    Checks if elm exists in path, else uses it from node_modules

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const path = require('path');
 const elmCompiler = require('node-elm-compiler');
+const cmdExists = require('command-exists').sync;
+
+const namespace = 'elm';
+const fileFilter = /\.elm$/;
+
+const getPathToElm = () =>
+  cmdExists('elm') ? 'elm' : './node_modules/.bin/elm';
 
 module.exports = () => ({
   name: 'elm',
   setup(build) {
-    const fileFilter = /\.elm$/;
-    const namespace = 'elm';
-    const options = { pathToElm: './node_modules/.bin/elm' };
+    const options = {
+      pathToElm: getPathToElm(),
+    };
 
     build.onResolve({ filter: fileFilter }, args => ({
       path: path.join(args.resolveDir, args.path),
       namespace,
     }))
 
-    build.onLoad({ filter: /.*/, namespace }, async (args) => {
+    build.onLoad({ filter: /.*/, namespace }, async args => {
       const contents = await elmCompiler.compileToString([args.path], options);
       return { contents };
     });
