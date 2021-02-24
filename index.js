3de194310ab3c4cf4dd5d45d7eada094a944a37e
Akshay Nair <phenax5@gmail.com>
2021-02-24 23:50:27 +0530

    Adds checks to see if elm is installed

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const path = require('path');
+const fs = require('fs');
 const elmCompiler = require('node-elm-compiler');
 const cmdExists = require('command-exists').sync;
 
 const namespace = 'elm';
 const fileFilter = /\.elm$/;
 
-const getPathToElm = () =>
-  cmdExists('elm') ? 'elm' : './node_modules/.bin/elm';
+const fileExists = p => fs.existsSync(p) && fs.statSync(p).isFile();
+
+const getPathToElm = () => {
+  if (fileExists('./node_modules/.bin/elm')) return [null, './node_modules/.bin/elm']
+  if (cmdExists('elm')) return [null, 'elm'];
+  return [new Error('Could not find `elm` executable. You can install it with `yarn add elm` or `npm install elm`'), null];
+};
 
 module.exports = () => ({
   name: 'elm',
   setup(build) {
+    const [error, pathToElm] = getPathToElm();
+    if (error) throw error;
+
     const options = {
-      pathToElm: getPathToElm(),
+      pathToElm,
     };
 
     build.onResolve({ filter: fileFilter }, args => ({
