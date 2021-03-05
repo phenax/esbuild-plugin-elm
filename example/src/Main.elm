cef98bd1e853826a3d8baa8e9ee4edb910e436f0
Akshay Nair <phenax5@gmail.com>
2021-03-05 15:24:35 +0530

    Updates example

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -4,31 +4,40 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
+
 main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
-type alias Model = Int
+
+type alias Model =
+    Int
+
 
 init : Model
-init = 0
+init =
+    0
+
 
 type Msg
-  = Increment
-  | Decrement
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
-    Decrement ->
-      model - 1
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
