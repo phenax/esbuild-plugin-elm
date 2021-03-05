8c009cf2d038c59850ab0e0119e0b7315d7bcfd2
Akshay Nair <phenax5@gmail.com>
2021-03-05 15:17:58 +0530

    Adds nix shell file

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {}, ... }:
+
+let
+  packages = with pkgs; [
+    elmPackages.elm
+    elmPackages.create-elm-app
+    elmPackages.elm-format
+    elmPackages.elm-language-server
+    nodejs-15_x
+    yarn
+  ];
+in
+pkgs.stdenv.mkDerivation {
+  name = "esbuild-plugin-elm";
+  buildInputs = packages;
+}
